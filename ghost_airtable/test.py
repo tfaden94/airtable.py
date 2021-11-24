92e8e919e1c76649d99c425e9b584b9f648b93b8
fadent <87021636+fadent@users.noreply.github.com>
2021-11-24 13:14:16 -0500

    config ignore

--- ghost_airtable/test.py
+++ ghost_airtable/test.py
@@ -1,6 +1,16 @@
 import airtable
+import json
 
-with open('/Users/tyler.faden/Git/Local/ghost/ghost_airtable/config/config.json', 'r')
+with open('/Users/tyler.faden/Git/Local/ghost/ghost_airtable/config/config.json', 'r') as file:
+    creds = json.load(file)
 
-at = airtable.Airtable('BASE_ID', 'API_KEY')
-at.get('TABLE_NAME')
+
+api_key = creds['API_KEY']
+base = creds['BASE']
+dp = base['Deal Pipeline']
+
+at = airtable.Airtable(dp, api_key)
+
+test = at.get('SALES DEALS (SELLER SIDE)')
+for i in test['records']:
+    print(i)
